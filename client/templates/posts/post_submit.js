dc16934d80235138ce07aee32699dd66fd10ef29
Ning, Tao <ningtaous@gmail.com>
2016-05-03 17:08:21 -0400

    add display errors on submit post

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,10 +20,14 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
     
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+    
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
       
       // show this result but route anyway
       if (result.postExists)
@@ -19,4 +36,3 @@ Template.postSubmit.events({
       Router.go('postPage', {_id: result._id});  
     });
   }
-});
