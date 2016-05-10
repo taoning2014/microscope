2f787aec2219112864b8edd409e646a3a9ab4e22
Ning, Tao <ningtaous@gmail.com>
2016-05-10 12:10:21 -0400

    Implement part 1 of detail page

--- server/publications.js
+++ server/publications.js
@@ -24,3 +24,8 @@ Meteor.publish('notifications', function() {
 Meteor.publish('groups', function() {
   return Groups.find({});
 });
+
+Meteor.publish('singleGroup', function(id) {
+  check(id, String);
+  return Groups.find({'_id': id});
+});
