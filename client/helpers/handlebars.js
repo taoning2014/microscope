4eb39687990a5147cf2534df094cd95d6af20f7f
Ning, Tao <ningtaous@gmail.com>
2016-05-10 11:55:11 -0400

    Refactor the display number of members

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -8,3 +8,15 @@ Template.registerHelper('pluralize', function(n, thing) {
     return n + ' ' + thing + 's';
   }
 });
+
+Template.registerHelper('pluralizeArray', function(array, thing) {
+  var n = array.length;
+  // fairly stupid pluralizer
+  if (n === 1) {
+    return '1 ' + thing;
+  } else if (n < 0){
+    return 0 + ' ' + thing;
+  } else {
+    return n + ' ' + thing + 's';
+  }
+});
