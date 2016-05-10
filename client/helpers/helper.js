d74eb44a5925fe75281fa43ea85cddda769a489c
Ning, Tao <ningtaous@gmail.com>
2016-05-10 15:00:43 -0400

    refactor user group

--- client/helpers/helper.js
+++ client/helpers/helper.js
@@ -2,6 +2,10 @@ Template.registerHelper('ownPost', function() {
 	return this.userId == Meteor.userId();
 });
 
+Template.registerHelper('ownGroup', function() {
+  return this.userId == Meteor.userId();
+});
+
 Template.registerHelper('upvotedClass', function(n, thing) {
     var userId = Meteor.userId();
     if (userId && !_.include(this.upvoters, userId)) {
